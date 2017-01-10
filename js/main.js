4085a95b916f37cdeb0bc3931d5e599660791591
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-10 10:57:21 -0800

    practicing constructor

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 setTimeout(function(){
    window.location.reload(1);
 }, 1000);
+
+// var customer =  {
+// 	name: "John Smith",
+// 	speak: function() {
+// 		return "My name is " + this.name;
+// 	},
+
+// 	address: {
+// 		street: '123 Main',
+// 		city: 'San Francisco',
+// 		state: 'CA'
+// 	}
+// }
