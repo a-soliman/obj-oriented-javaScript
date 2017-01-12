06ed70e914762bc0d5a8423b02880e37da8fd818
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-11 20:33:52 -0800

    getters and seters

--- js/main.js
+++ js/main.js
@@ -140,3 +140,22 @@ setTimeout(function(){
 // }
 
 // document.write("The Secret Number is " + secret.getSecret() + "<br />")
+
+	// === getters and seters ===
+
+// var address = {
+// 	street: "No Street",
+// 	city: "No City",
+// 	state: "No State",
+
+// 	get getAddress() {
+// 		return this.street + ", " + this.city + ", " + this.state + "."
+// 	},
+
+// 	set setAddress(theAddress) {
+// 		var parts = theAddress.toString().split(", ")
+// 		this.street = parts[0] || "";
+// 		this.city = parts[1] || "" ;
+// 		this.state = parts[2] || "";
+// 	}
+// }
