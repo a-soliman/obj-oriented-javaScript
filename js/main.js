1608a4904dcf4aceb12cb1c3731303716142fe55
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-01-12 20:39:52 -0800

    creating getter and setter bassed on ECMA Script 5.1v

--- js/main.js
+++ js/main.js
@@ -209,3 +209,21 @@ setTimeout(function(){
 // aPoint.pointPos = "100, 200";
 
 // document.write("point Position " + aPoint.pointPos + "<br />")
+
+// creating getter and setter bassed on ECMA Script 5.1v
+
+// var Circle = function(radius) {
+// 	this._radius = radius
+// }
+
+// Circle.prototype = {
+// 	set radius(radius) {this._radius = radius;},
+// 	get radius() { return this._radius;},
+// 	get area() { return Math.PI * (this._radius * this._radius);}
+// };
+
+// var circ = new Circle(10)
+
+// circ.radius = 15;
+
+// document.write('A circle with radius : ' + circ.radius + " has an area of : " +circ.area.toFixed(2) + "<br />")
